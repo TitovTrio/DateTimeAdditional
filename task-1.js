e20daaf254917385e2aad53d584391d867423802
TitovTrio <titovtrio@gmail.com>
2024-08-17 00:12:18 +0300

    Add files via upload

---
+++ task-1.js
@@ -0,0 +1,42 @@
+function allowVisa(people) {
+    return peopleWithVisa.filter(person => {
+        if (Date.now() < passportExpirationDateParser(person.passportExpiration)) {
+            return !person.criminalRecord;
+        }
+    });
+}
+
+function passportExpirationDateParser(stringDate) {
+    return Date.parse(stringDate.split(".").reverse().join("-"));
+}
+
+const peopleWithVisa = [
+  {
+      firstName: 'Stasia',
+      lastName: 'Ward',
+      criminalRecord: true,
+      passportExpiration: '19.06.2040',
+  },
+  {
+      firstName: 'Elliot',
+      lastName: 'Baker',
+      criminalRecord: false,
+      passportExpiration: '04.06.2041',
+  },
+  {
+      firstName: 'Leighann',
+      lastName: 'Scott',
+      criminalRecord: true,
+      passportExpiration: '31.07.2039',
+  },
+  {
+      firstName: 'Nick',
+      lastName: 'Pop',
+      criminalRecord: false,
+      passportExpiration: '31.12.2010',
+  },
+];
+
+const result = allowVisa(peopleWithVisa);
+console.log(peopleWithVisa)
+console.log('result', result);
